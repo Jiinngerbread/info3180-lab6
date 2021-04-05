9d975b5b60ed4e3b06eb2136ea3147f8b98b9a46
jheanel.brown <Jiinngerbread@gmail.com>
2021-04-05 16:19:07 -0500

    updated with routing, updated the index.html as well, had to rearrange order of app.js

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,57 +1,21 @@
 /* Add your Application JavaScript */
-const app = Vue.createApp({
+const Home = {
+  name: 'Home',
+  template: `
+    <div class="home">
+      <img src="/static/images/logo.png" alt="VueJS Logo">
+      <h1>{{ welcome }}</h1>
+    </div>
+  `,
   data() {
     return {
       welcome: 'Hello World! Welcome to VueJS'
     }
   }
-});
+};
 
-app.component('app-header', {
-  name: 'AppHeader',
-  template: `
-      <header>
-          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-            <a class="navbar-brand" href="#">VueJS App</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-      </header>    
-  `,
-  data: function() {
-    return {};
-  }
-});
-
-app.component('app-footer', {
-  name: 'AppFooter',
-  template: `
-      <footer>
-          <div class="container">
-              <p>Copyright &copy {{ year }} Flask Inc.</p>
-          </div>
-      </footer>
-  `,
-  data: function() {
-      return {
-          year: (new Date).getFullYear()
-      }
-  }
-});
-
-app.component('news-list', {
+const NewsList = {
   name: 'NewsList',
   template: `
     <div class="news">
@@ -76,10 +40,10 @@ app.component('news-list', {
             <p class="card-text">{{ article.description }}</p>
           </div>
         </div>
+      </div>
     </div>
-  </div>
-  
-      
+  </div> 
+  </div>  
   `,
   created() {
     let self= this;
@@ -121,6 +85,78 @@ app.component('news-list', {
           });
       }
   }
+};
+
+
+const app = Vue.createApp({
+  data() {
+    return {
+      welcome: 'Hello World! Welcome to VueJS'
+    }
+  },
+  components: {
+    'home' : Home,
+    'news-list': NewsList
+  }
 });
+
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/news', component: NewsList }
+  ]
+});
+
+
+app.component('app-header', {
+  name: 'AppHeader',
+  template: `
+      <header>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+            <a class="navbar-brand" href="#">VueJS App</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <router-link to="/" class="nav-link">Home</router-link>  
+                </li>
+                <li class="nav-item">
+                <router-link to="/news" class="nav-link">News</router-link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+      </header>    
+  `,
+  data: function() {
+    return {};
+  }
+});
+
+
+app.component('app-footer', {
+  name: 'AppFooter',
+  template: `
+      <footer>
+          <div class="container">
+              <p>Copyright &copy {{ year }} Flask Inc.</p>
+          </div>
+      </footer>
+  `,
+  data: function() {
+      return {
+          year: (new Date).getFullYear()
+      }
+  }
+});
+
+
+app.use(router);
+
 
 app.mount('#app');
